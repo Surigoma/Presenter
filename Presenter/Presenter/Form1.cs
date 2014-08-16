f4ffbfe1583bbf2a8783a717c58b250e151be166
Surigoma <admin@mail.surigoma.net>
2014-08-16 16:46:35 +0900

    Add ProjectFiles
    and editting README.md

---
+++ Presenter/Presenter/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presenter
+{
+    public partial class Form1 : Form
+    {
+        Viewer v;
+        int ScreenViewNum = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            v = new Viewer(ScreenViewNum);
+            v.Show();
+            v.FormClosed += v_FormClosed;
+        }
+
+        void v_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (v != null)
+                v.Dispose();
+            v = new Viewer(ScreenViewNum);
+            v.Show();
+            v.FormClosed += v_FormClosed;
+        }
+    }
+}
---
+++ Presenter/Presenter/Viewer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presenter
+{
+    public partial class Viewer : Form
+    {
+        public Viewer(int ScreenNum)
+        {
+            InitializeComponent();
+            var r = Screen.AllScreens[ScreenNum].Bounds;
+            this.Location = new Point(r.X, r.Y);
+            this.Width = r.Width;
+            this.Height = r.Height;
+        }
+    }
+}
